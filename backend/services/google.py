b6ee8a9a1f0e78facab1797db557a6464238e406
Juan Cruz Saldaña <juancruzsaldana14@gmail.com>
2022-03-07 22:48:06 -0300

    Refactor: sheets creation

--- backend/services/google.py
+++ backend/services/google.py
@@ -1,3 +1,4 @@
+import re
 import gspread
 from dotenv import load_dotenv, find_dotenv
 import os
@@ -17,27 +18,38 @@ class GoogleService:
             return False
         return worksheet.get_all_values()
 
+    def removeSpecialChars(self, text):
+        return re.sub('[^A-Za-z0-9]+', '', text)
+
+    def createSheets (self, file, data):
+        sh = self.gc.open(file)
+        existingWorksheets = list(map(lambda x: x.title, sh.worksheets()))
+        for sheet in data:
+            sheet = 'Total' if sheet == 'Totales'  else sheet
+            sheet = self.removeSpecialChars(sheet)
+            if sheet not in existingWorksheets:
+                sh.add_worksheet(sheet, 100, 20)
+        return sh
+       
+
     def update_google_sheet(self, file, data):
-        rate = data['rate'];
-        
+        rate = data['rate'];  
         data = data['structure']
         try:
-            sh = self.gc.open(file)
-            existingWorksheets = list(map(lambda x: x.title, sh.worksheets()))
-            
+            sh = self.createSheets(file, data)
             for sheet in data:
                 value = data[sheet] 
                 sheet = 'Total' if sheet == 'Totales'  else sheet
-                worksheet = sh.worksheet(sheet) if sheet in existingWorksheets else sh.add_worksheet(sheet, 100, 20)
-
+                sheetName = self.removeSpecialChars(sheet)
+                worksheet = sh.worksheet(sheetName)
                 rows = []
                 startRow = 2
                 endRow = startRow
 
                 for task in value['tasks']:
-                    text = task['description']
-                    formula = f'={text}!F3'
-                    row = [text, formula] if sheet == 'Total' else [task['date'], task['description'], task['duration']]
+                    sheetReference = self.removeSpecialChars(task['description'])
+                    formula = f'={sheetReference}!F3'
+                    row = [task['description'], formula] if sheet == 'Total' else [task['date'], task['description'], task['duration']]
                     rows.append(row)
                     endRow += 1
                 
@@ -47,14 +59,15 @@ class GoogleService:
                 rateCell = 'B1' if sheet == 'Total' else 'F1'
                 worksheet.update(rateCell, rate,value_input_option='USER_ENTERED')
                 sumFormula = f'=SUM(B{startRow}:B{endRow-1}) * B1' if sheet == 'Total' else f'=(INDEX(split(SUM(C{startRow}:C{endRow-1}),":"),1))+((INDEX(split(SUM(C{startRow}:C{endRow-1}),":"),2)/30)*0.5)'
-                sumCell = f'A{endRow + 3}' if sheet == 'Total' else 'F3'
+                sumCell = f'B{endRow + 3}' if sheet == 'Total' else 'F3'
 
                 if sheet == 'Total':
                     worksheet.update(sumCell, sumFormula, value_input_option='USER_ENTERED')
                 else:
                     worksheet.update(sumCell, sumFormula, raw=False)
-                    worksheet.update('F5', '=F3*F1', raw=False)                                       
+                    worksheet.update('F5', '=F3*F1', raw=False)
+                                  
         except Exception as e:
-            print(e)
+            print(str(e))
             return False
         return True
