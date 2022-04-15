b706e12938f424a3cc7c36a8ee98d8679229fed9
Juan Cruz Saldaña <juancruzsaldana14@gmail.com>
2022-04-15 20:25:40 -0300

    Feat: home section

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -5,6 +5,7 @@
 	import appConfigService from './services/appConfigService';
 	import Sidebar from './Components/Sidebar.svelte';
 	import Access from './Access/Access.svelte';
+	import Home from './Home/Home.svelte';
 	import 'flowbite';
 	import Dollars from './Dollars/Dollars.svelte';
 	let {sidebarItems, home} = appConfigService.getFeatures();
@@ -16,14 +17,9 @@
 <Router>
 	<div class="w-full flex flex-col sm:flex-row flex-grow">
 		<Sidebar items={sidebarItems} home={home}/>
-		<Route path="access">
-			<div class="all-screen min-h-screen w-full flex-grow pt-4 dark:bg-gradient-to-br from-stone-900 via-stone-900 to-stone-800 ">
-				<Access/>
-			</div>
-		</Route>
-		<Route path="expenses">
-			<div class="all-screen min-h-screen w-full flex-grow pt-4 dark:bg-gradient-to-br from-neutral-900 via-neutral-900 to-neutral-800 ">
-				<Expenses rate={rate}/>
+		<Route path="/">
+			<div class="flex flex-col all-screen min-h-screen w-full flex-grow pt-4 dark:bg-gradient-to-br from-zinc-900 via-zinc-900 to-zinc-800 ">
+				<Home sections={sidebarItems} home={home}/>
 			</div>
 		</Route>
 		<Route path="tasks">
@@ -31,7 +27,17 @@
 				<TasksTable rate={rate}/>
 			</div>
 		</Route>
-		<Route path="/">
+		<Route path="expenses">
+			<div class="all-screen min-h-screen w-full flex-grow pt-4 dark:bg-gradient-to-br from-neutral-900 via-neutral-900 to-neutral-800 ">
+				<Expenses rate={rate}/>
+			</div>
+		</Route>
+		<Route path="access">
+			<div class="all-screen min-h-screen w-full flex-grow pt-4 dark:bg-gradient-to-br from-stone-900 via-stone-900 to-stone-800 ">
+				<Access/>
+			</div>
+		</Route>
+		<Route path="dollars">
 			<div class="all-screen min-h-screen w-full flex-grow pt-4 dark:bg-gradient-to-br from-emerald-900 via-emerald-900 to-emerald-800">
 				<Dollars/>	
 			</div>
---
+++ frontend/src/Home/Home.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {Link} from 'svelte-navigator';
+    import Icons from '../Icons/Icons.svelte';
+    export let sections;
+    export let home
+</script>
+
+<h1 class="text-center font-bold text-lg  text-zinc-600">WELCOME!</h1>
+<div class="flex justify-around items-center flex-grow flex-wrap">
+    {#each sections as section }
+        {#if section !== home}
+            <div class="card flex flex-col justify-between rounded-lg shadow-md dark:border-gray-700 w-1/5 saturate-50 hover:saturate-150 relative">
+                <img class="rounded-t-lg object-fill h-48 w-full" src="./imgs/{section}.jpg" alt={section} />
+                <Link title="{section.toUpperCase() + section.slice(1)}"  to="/{section}"
+                        class="after:absolute after:inset-0 flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-b-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <Icons name={section} tailwind="w-6 h-6 text-white"/>
+                            <span class={`capitalize text-white ml-2`}>{section}</span>
+                </Link>
+            </div>
+        {/if}
+        
+    {/each}
+</div>
